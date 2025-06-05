782184fd57d54519971d8079e37e94a009ece2c7
Muelvzz <muelvinlopez25@gmail.com>
2025-06-05 17:45:15 +0800

    created a table to display to the pdf

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import pandas as pd
 import glob
-import openpyxl
 from fpdf import FPDF
 from pathlib import Path
 
 filepath = glob.glob("invoices/*.xlsx")
 
-pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.set_auto_page_break(auto=False, margin=0)
-
 for file in filepath:
-    df = pd.read_excel(file, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.set_auto_page_break(auto=False, margin=0)
 
     pdf.add_page()
 
     filename = Path(file).stem
-    invoice_nr = filename.split("-")
+    invoice_nr, date = filename.split("-")
 
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Invoice Nr. {invoice_nr[0]}", align="L", ln=1)
-    pdf.cell(w=0, h=5, txt=invoice_nr[1], align="L", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice Nr. {invoice_nr}", align="L", ln=1)
+    pdf.cell(w=0, h=5, txt=f"Date {date}", align="L", ln=1)
+
+    df = pd.read_excel(file, sheet_name="Sheet 1")
+
+    columns = list(df.columns)
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times",style="B", size=10)
+
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=50, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=40, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"pdf's\{filename}.pdf")
